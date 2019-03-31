941e58a1f58d640a4adf0840a2f5c56e5b2e6357
ikura1 <ikura.lab@gmail.com>
2019-03-31 21:37:31 +0900

    refactor: 2進数で解答されてた なるほど

--- beginer_selection/ABC081B.py
+++ beginer_selection/ABC081B.py
@@ -3,22 +3,10 @@ import sys
 input = sys.stdin.readline
 
 
-def waru(a_list, n=0):
-    hoge = []
-    for a in a_list:
-        q, mod = divmod(a, 2)
-        if mod == 1:
-            return n
-        hoge.append(q)
-    return waru(hoge, n + 1)
-
-
 def main():
     n = int(input())
     a_list = [int(i) for i in input().split()]
-    # 全探索するなら
-
-    print(waru(a_list))
+    print(min([len(bin(i).split("1")[-1]) for i in a_list]))
 
 
 if __name__ == "__main__":
