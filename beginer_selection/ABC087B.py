e0cae440a0c3ef3d0e34ac2f14b385e9665f55f8
ikura1 <ikura.lab@gmail.com>
2019-03-31 22:16:17 +0900

    ABC087Bを全探索でやった

---
+++ beginer_selection/ABC087B.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import product
+
+input = sys.stdin.readline
+
+
+def main():
+    a = [i * 500 for i in range(int(input()) + 1)] or [0]
+    b = [i * 100 for i in range(int(input()) + 1)] or [0]
+    c = [i * 50 for i in range(int(input()) + 1)] or [0]
+    x = int(input())
+    all_ = [[i, j, k] for i in a for j in b for k in c]
+    # pro = [[i, j, k] for i, j, k in product(a, b, c)]
+    # print(len(all_), len(pro))
+    print(len([i for i in all_ if sum(i) == x]))
+
+
+if __name__ == "__main__":
+    main()
